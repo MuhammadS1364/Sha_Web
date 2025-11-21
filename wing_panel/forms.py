4ee532459171d42e8fcd2fcd1ba58d9aa9040cf3
MuhammadS1364 <shafeealam1364@outlook.com>
2025-11-21 21:58:53 +0530

    Form Cleaned

--- wing_panel/forms.py
+++ wing_panel/forms.py
@@ -1,18 +1,18 @@
-# from django import forms
+from django import forms
 
-# from .models import *
+from .models import *
 
-# class Add_Program_Form(forms.ModelForm):
-#     class Meta:
-#         model = Program_Bank
-#         exclude = ["Program_Created"]
-#         widgets = {
-#             "Program_date": forms.DateInput(attrs={'type': 'date'}),
-#             "Program_poster": forms.ClearableFileInput(attrs={'multiple': True}),               
+class Add_Program_Form(forms.ModelForm):
+    class Meta:
+        model = Program_Bank
+        exclude = ["Program_Created"]
+        widgets = {
+            "Program_date": forms.DateInput(attrs={'type': 'date'}),
+            "Program_poster": forms.ClearableFileInput(attrs={'class': 'form-control'}),               
             
-#         }
-# class Registration_Programe_Form(forms.ModelForm):
-#     class Meta:
-#         model = Candidates_Registration_Model
-#         exclude = ["Candidates_Name", "Registered_Programe"]
+        }
+class Registration_Programe_Form(forms.ModelForm):
+    class Meta:
+        model = Candidates_Registration_Model
+        exclude = ["Candidates_Name", "Registered_Programe"]
 
